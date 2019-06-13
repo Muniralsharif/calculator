544f7a245375ac5167683c60ffcedce3192d982d
Muniralsharif <44524993+Muniralsharif@users.noreply.github.com>
2019-06-13 15:12:31 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 
 
-var num1='';
-var num2='';
+
 var opp="";
 
 const zero=document.querySelector(".a0");
@@ -15,14 +14,41 @@ const seven=document.querySelector(".a7");
 const eight=document.querySelector(".a8");
 const nine=document.querySelector(".a9");
 const dot= document.querySelector(".dot")
-var result = document.querySelector("h1");
-var clear = document.querySelector(".clear");
+const result = document.querySelector("h1");
+const clear = document.querySelector(".clear");
+const backspace = document.querySelector(".backspace");
 
+// const numbers = document.querySelectorAll(".numx");
+// for(var i=0;i<numbers.length;i++)
+// {numbers[i].addEventListener('click',function(event){
+
+
+
+//     result.innerText+=event.srcElement.innerText;
+
+
+
+// });
+
+// }
+backspace.addEventListener('click',function(){
+
+
+result.innerText=result.innerText.substring(0, result.innerText.length-1);
+
+
+
+
+
+
+
+
+});
 
 
 zero.addEventListener('click',function(){
 
-    num1+=0;
+    
     result.innerText+=0;
     
     
@@ -30,71 +56,71 @@ zero.addEventListener('click',function(){
     
     one.addEventListener('click',function(){
     
-    num1+=1;
+    
     result.innerText+=1;
     
     
     });
     two.addEventListener('click',function(){
     
-    num1+=2;
+    
     result.innerText+=2;
     
     
     });
     three.addEventListener('click',function(){
     
-    num1+=3;
+   
     result.innerText+=3;
     
     
     });
     four.addEventListener('click',function(){
     
-    num1+=4;
+    
     result.innerText+=4;
     
     
     });
     five.addEventListener('click',function(){
     
-    num1+=5;
     result.innerText+=5;
     
     
     });
     six.addEventListener('click',function(){
     
-    num1+=6;
+    
     result.innerText+=6;
     
     
     });
     seven.addEventListener('click',function(){
     
-    num1+=7;
+    
     result.innerText+=7;
     
     
     });
     eight.addEventListener('click',function(){
     
-    num1+=8;
+   
     result.innerText+=8;
     
     
     });
     nine.addEventListener('click',function(){
     
-    num1+=9;
+ 
     result.innerText+=9;
     
     
     });
     dot.addEventListener('click',function(){
         console.log("dot pressed");
-    num1+=".";
-    result.innerText+=".";
+   var  cDot=".";
+    dotCheck(cDot);
+    
     
     
     });
@@ -107,57 +133,79 @@ var subtract=document.querySelector(".subtract");
 var add=document.querySelector(".add");
 var equal=document.querySelector(".ae");
 
-divide.addEventListener('click',function(){
-num2=num1;
-num1=0;
-opp="/";
-result.innerText+=opp;
+divide.addEventListener('click',function(){ 
 
+opp="/";
+checkOpAdd(opp);
+
+   
 });
 multiply.addEventListener('click',function(){
-num2=num1;
-num1=0;
+
 opp="*";
-result.innerText+=opp;
+checkOpAdd(opp);
+
 
 });
 subtract.addEventListener('click',function(){
-num2=num1;
-num1=0;
+
 opp="-";
-result.innerText+=opp;
+checkOpAdd(opp);
+
 
 });
 add.addEventListener('click',function(){
-num2=num1;
-num1=0;
+
 opp="+";
-result.innerText+=opp;
+checkOpAdd(opp);
+
 
 });
 
 equal.addEventListener('click',function(){
+    // read from screen :)
+    var str='';
+    var s='';
+    var lastResult='';
+    str=result.innerText;
+    var arr1=[];
+    var arr2=[];
 
-if(opp==="+"){
-
-    result.innerText=Number(num1)+Number(num2);
-
-}else if(opp==="-"){
-
-result.innerText=Number(num2)-Number(num1);
-
-
-}else if(opp==="*"){
-
-result.innerText=Number(num1)*Number(num2);
-
-
-}else if(opp==="/"){
-
-result.innerText=Number(num2)/Number(num1);
+for(var i=0;i<str.length;i++)
+{ if(str[i]==='+'||str[i]==='-'||str[i]==='*'||str[i]==='/')
+     {
+       arr1.push(s);
+       arr2.push(str[i]);
+     
+     s='';
+     }
+     else
+     s=s+str[i];
 
 
 }
+arr1.push(s);
+// retrive and calculate the result :()
+for(var i=0;i<arr2.length;i++)
+{
+    if(arr2[i]==='+')
+    arr1[i+1]=Number(arr1[i])+Number(arr1[i+1])
+    else if(arr2[i]==='-')
+    arr1[i+1]=Number(arr1[i])-Number(arr1[i+1])
+    else if(arr2[i]==='*')
+    arr1[i+1]=Number(arr1[i])* Number(arr1[i+1])
+    else if(arr2[i]==='/')
+    arr1[i+1]=Number(arr1[i])/Number(arr1[i+1])
+
+    lastResult=(arr1[arr1.length-1]);
+    
+    
+
+
+
+}
+result.innerText=lastResult;
+
 
 
 
@@ -166,8 +214,7 @@ result.innerText=Number(num2)/Number(num1);
 clear.addEventListener('click',function(){
 
     result.innerText='';
-     num1='';
-     num2='';
+    
      opp="";
 
 
@@ -178,6 +225,44 @@ clear.addEventListener('click',function(){
 
 
 });
+function checkOpAdd(opp){
+    var strx=result.innerText;
+if(strx.charAt(strx.length-1)==='+'||strx.charAt(strx.length-1)==='-'||strx.charAt(strx.length-1)==='*'||strx.charAt(strx.length-1)==='/')
+{
+
+    
+    strx = strx.slice(0, -1) + opp;
+    result.innerText=strx;
+
+
+}else{
+    
+    result.innerText+=opp;
+
+     }
+
+}
+function dotCheck(cDot)
+{
+var strx=result.innerText;
+
+    if(strx.charAt(strx.length-1)!='.'||strx.contains("\\."==false))
+    {
+        result.innerText+=cDot;
+        
+        
+    
+    
+    }else{
+        
+        strx = strx.slice(0, -1) + cDot;
+        result.innerText=strx;
+    
+         }
+    
+    }
+
+
 
 
 
